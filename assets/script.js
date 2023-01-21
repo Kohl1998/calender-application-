800e4adc213dc9406fb2018f3a24cf5f52f9a92b
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-01-21 13:46:46 +0000

    selected container div/created array of times

--- assets/script.js
+++ assets/script.js
@@ -7,4 +7,20 @@ currentTime.text(moment().format("DDD, MMM, YYYY, hh:mm:ss", 1000));
 // Appends it to text field 
 currentTime.appendTo('.currentDay')
 
+// To select div container
+var timeBlocks = $('.container');
+
+var Times = [
+    "9am",
+    "10am",
+    "11am",
+    "12pm",
+    "1pm",
+    "2pm",
+    "3pm",
+    "4pm",
+    "5pm"
+]
+
+
 
