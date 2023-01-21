f994e2570559b695b4d0a225923bd6722f1679b5
kohlvbusiness98@gmail.com <kohlvbusiness98@gmail.com>
2023-01-21 14:47:31 +0000

    set classes for elements to apply css styles

--- assets/script.js
+++ assets/script.js
@@ -22,5 +22,31 @@ var Times = [
     "5pm"
 ]
 
+// Selectors for timeblocks
+var nineAM = $('#9am');
+var tenAm = $('#10am');
+var elevenAm = $('#11am');
+var Twelve = $('#12pm');
+var onePM = $('#1pm') ;
+var twoPm = $('#2pm');
+var threePM = $('#3pm');
+var fourPM = $('#4pm');
+var fivePM = $('#5pm'); 
 
 
+nineAM.text(Times[0]).attr("class", "hour")
+tenAm.text(Times[1]).attr("class", "hour")
+elevenAm.text(Times[2]).attr("class", "hour")
+Twelve.text(Times[3]).attr("class", "hour")
+onePM.text(Times[4]).attr("class", "hour")
+twoPm.text(Times[5]).attr("class", "hour")
+threePM.text(Times[6]).attr("class", "hour")
+fourPM.text(Times[7]).attr("class", "hour")
+fivePM.text(Times[8]).attr("class", "hour")
+
+
+
+/* for  (var i = 0; i < Times.length; i++) {
+    var timeHolders = $('<p>');
+    timeHolders.text(Times[i]).appendTo('div');
+} */
